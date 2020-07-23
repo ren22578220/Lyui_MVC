4308ab0e4893683113493b1c803896869801f1eb
利强 任 <2113312023@qq.com>
2020-07-23 08:38:04 +0800

    添加项目文件。

---
+++ Lyui_MVC/Controllers/ClssController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lyui_MVC.Controllers
+{
+    public class ClssController : Controller
+    {
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
